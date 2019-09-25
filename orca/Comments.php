dee36b6a78834a3120bdaf662280ad19a07d831b
JuliusM5 <54817220+JuliusM5@users.noreply.github.com>
2019-09-25 16:02:50 +0300

    Add files via upload

---
+++ orca/Comments.php
@@ -0,0 +1,53 @@
+<?php
+function createCommentRow($data) {
+    global $conn;
+
+    $response = '
+            <div class="comment">
+                <div class="user">'.$data['name'].' <span class="time">'.$data['createdOn'].'</span></div>
+                <div class="userComment">'.$data['comment'].'</div>
+                <div class="reply"><a href="javascript:void(0)" data-commentID="'.$data['id'].'" onclick="reply(this)">Atsakyti</a></div>
+                <div class="replies">';
+//    var_dump($data['id']);
+    $sql = $conn->query("SELECT replies.id, name, comment, DATE_FORMAT(replies.createdOn, '%Y-%m-%d') AS createdOn FROM replies INNER JOIN users ON replies.userID = users.id WHERE replies.commentID = '".$data['id']."' ORDER BY replies.id DESC LIMIT 1");
+
+
+    while($dataR = $sql->fetch_assoc())
+        $response .= createCommentRow($dataR);
+
+    $response .= '
+                        </div>
+            </div>
+        ';
+
+    return $response;
+}
+
+if (isset($_POST['getAllComments'])) {
+    $start = $conn->real_escape_string($_POST['start']);
+
+    $response = "";
+    $sql = $conn->query("SELECT comments.id, name, comment, DATE_FORMAT(comments.createdOn, '%Y-%m-%d') AS createdOn FROM comments INNER JOIN users ON comments.userID = users.id ORDER BY comments.id DESC LIMIT $start, 20");
+    while($data = $sql->fetch_assoc())
+        $response .= createCommentRow($data);
+
+    exit($response);
+}
+
+if (isset($_POST['addComment'])) {
+    $comment = $conn->real_escape_string($_POST['comment']);
+    $isReply = $conn->real_escape_string($_POST['isReply']);
+    $commentID = $conn->real_escape_string($_POST['commentID']);
+
+    if ($isReply != 'false') {
+        $conn->query("INSERT INTO replies (comment, commentID, userID, createdOn) VALUES ('$comment', '$commentID', '".$_SESSION['userID']."', NOW())");
+        $sql = $conn->query("SELECT replies.id, name, comment, DATE_FORMAT(replies.createdOn, '%Y-%m-%d') AS createdOn FROM replies INNER JOIN users ON replies.userID = users.id ORDER BY replies.id DESC LIMIT 1");
+    } else {
+        $conn->query("INSERT INTO comments (userID, comment, createdOn) VALUES ('".$_SESSION['userID']."','$comment',NOW())");
+        $sql = $conn->query("SELECT comments.id, name, comment, DATE_FORMAT(comments.createdOn, '%Y-%m-%d') AS createdOn FROM comments INNER JOIN users ON comments.userID = users.id ORDER BY comments.id DESC LIMIT 1");
+    }
+
+    $data = $sql->fetch_assoc();
+    exit(createCommentRow($data));
+}
+
---
+++ orca/index.php
@@ -0,0 +1,232 @@
+<?php
+session_start();
+
+$loggedIn = false;
+
+if (isset($_SESSION['loggedIn']) && isset($_SESSION['name'])) {
+    $loggedIn = true;
+}
+
+$conn = new mysqli('localhost', 'root', '', 'test1');
+
+?>
+<?php  include "Comments.php"?>
+<?php  include "Register.php"?>
+<?php  include "Login.php"?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Komentarai</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<!--Registracijos dalis-->
+<div class="modal" id="registerModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Registracijos forma</h5>
+            </div>
+            <div class="modal-body">
+                <input type="text" id="userName" class="form-control" placeholder="Vardas">
+                <input type="email" id="userEmail" class="form-control" placeholder="Email">
+                <input type="password" id="userPassword" class="form-control" placeholder="Slaptažodis">
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-primary" id="registerBtn">Registracija</button>
+                <button class="btn btn-default" data-dismiss="modal">Uždaryti</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Prisijungimo dalis-->
+<div class="modal" id="logInModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Prisijungimo forma</h5>
+            </div>
+            <div class="modal-body">
+                <input type="email" id="userLEmail" class="form-control" placeholder="Email">
+                <input type="password" id="userLPassword" class="form-control" placeholder="Slaptažodis">
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-primary" id="loginBtn">Prisijungti</button>
+                <button class="btn btn-default" data-dismiss="modal">Atsijungti</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Komentarų dalis-->
+<div class="container" style="margin-top:50px;">
+    <div class="row">
+        <div class="col-md-12" align="right">
+            <?php
+            if (!$loggedIn)
+                echo '
+                        <button class="btn btn-primary" data-toggle="modal" data-target="#registerModal">Registracija</button>
+                        <button class="btn btn-success" data-toggle="modal" data-target="#logInModal">Prisijungimas</button>
+                ';
+            else
+                echo '
+                    <a href="logout.php" class="btn btn-warning">Log Out</a>
+                ';
+            ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <textarea class="form-control" id="mainComment" placeholder="Pridėti komentarą" cols="30" rows="2"></textarea><br>
+            <button style="float:right" class="btn-primary btn" onclick="isReply = false;" id="addComment">Pridėti komentarą</button>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <h2><b id="numComments"><?php echo $numComments ?> Komentarai</b></h2>
+            <div class="userComments">
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row replyRow" style="display:none">
+    <div class="col-md-12">
+        <textarea class="form-control" id="replyComment" placeholder="Pridėkite komentarą" cols="30" rows="2"></textarea><br>
+        <button style="float:right" class="btn-primary btn" onclick="isReply = true;" id="addReply">Atsakyti</button>
+        <button style="float:right" class="btn-default btn" onclick="$('.replyRow').hide();">Uždaryti</button>
+    </div>
+</div>
+
+<script src="http://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script type="text/javascript">
+    var isReply = false, commentID = 0, max = <?php echo $numComments ?>;
+
+    $(document).ready(function () {
+        $("#addComment, #addReply").on('click', function () {
+            var comment;
+
+            if (!isReply)
+                comment = $("#mainComment").val();
+            else
+                comment = $("#replyComment").val();
+
+            if (comment.length > 5) {
+                $.ajax({
+                    url: 'index.php',
+                    method: 'POST',
+                    dataType: 'text',
+                    data: {
+                        addComment: 1,
+                        comment: comment,
+                        isReply: isReply,
+                        commentID: commentID
+                    }, success: function (response) {
+                        max++;
+                        $("#numComments").text(max + "comment");
+
+                        if (!isReply) {
+                            $(".userComments").prepend(response);
+                            $("#mainComment").val("");
+                        } else {
+                            commentID = 0;
+                            $("#replyComment").val("");
+                            $(".replyRow").hide();
+                            $('.replyRow').parent().next().append(response);
+                        }
+                    }
+                });
+            } else
+                alert('Patikrinkite ar viską gerai suvedėte');
+        });
+
+        $("#registerBtn").on('click', function () {
+            var name = $("#userName").val();
+            var email = $("#userEmail").val();
+            var password = $("#userPassword").val();
+
+            if (name != "" && email != "" && password != "") {
+                $.ajax({
+                    url: 'index.php',
+                    method: 'POST',
+                    dataType: 'text',
+                    data: {
+                        register: 1,
+                        name: name,
+                        email: email,
+                        password: password
+                    }, success: function (response) {
+                        if (response === 'failedEmail')
+                            alert('Įveskite email!');
+                        else if (response === 'failedUserExists')
+                            alert('Šis vartotojas egzistuoja!');
+                        else
+                            window.location = window.location;
+                    }
+                });
+            } else
+                alert('Patikrinkite ar viską gerai suvedėte');
+        });
+
+        $("#loginBtn").on('click', function () {
+            var email = $("#userLEmail").val();
+            var password = $("#userLPassword").val();
+
+            if (email != "" && password != "") {
+                $.ajax({
+                    url: 'index.php',
+                    method: 'POST',
+                    dataType: 'text',
+                    data: {
+                        logIn: 1,
+                        email: email,
+                        password: password
+                    }, success: function (response) {
+                        if (response === 'failed')
+                            alert('Patikrinkite Login duomenis!');
+                        else
+                            window.location = window.location;
+                    }
+                });
+            } else
+                alert('Patikrinkite ar viską gerai suvedėte');
+        });
+
+        getAllComments(0, max);
+    });
+
+    function reply(caller) {
+        commentID = $(caller).attr('data-commentID');
+        $(".replyRow").insertAfter($(caller));
+        $('.replyRow').show();
+    }
+
+    function getAllComments(start, max) {
+        if (start > max) {
+            return;
+        }
+
+        $.ajax({
+            url: 'index.php',
+            method: 'POST',
+            dataType: 'text',
+            data: {
+                getAllComments: 1,
+                start: start
+            }, success: function (response) {
+                $(".userComments").append(response);
+                getAllComments((start+20), max);
+            }
+        });
+    }
+</script>
+</body>
+</html>
